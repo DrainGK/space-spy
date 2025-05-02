c11f590bbe1975613d1768f49e3495d6ae9c0a40
ロペズ ギヨム <guillaumelopezgk@hotmail.com>
2025-05-02 17:45:56 +0900

    Ouiii

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -15,8 +15,8 @@ const Header = () => {
     <header className="relative w-full  px-5 pb-15 bg-[url(/assets/bg_sky.webp)] bg-cover backdrop-blur">
         <div className="flex">
             <img 
-                className="max-w-full h-fit block z-10"
-                src="/assets/title-vertical.webp" alt="title" height="153.55" width="492"/>
+                className="max-w-full w-[154px] w-[] block z-10"
+                src="/assets/title-vertical.png" alt="title" height="492" width="154"/>
             <img src="/assets/spy_dance_1.webp " className="max-w-full h-fit block z-10" alt="spys" height="306" width="400"/>
         </div>
         <img 
